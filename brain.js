a0b7ea7f57f6533dc4bf272f5514bec08fd4fff3
James <james.hudgins.work@gmail.com>
2020-06-26 20:30:25 -0400

    location on load fixed based on ip and geo location

--- brain.js
+++ brain.js
@@ -1,11 +1,37 @@
 $(document).ready(function () {
-// define list containing of cities
+    // First get the client ip address
+    $.getJSON('https://api.ipinfodb.com/v3/ip-city/?key=<5751d8bc1f2eb9ccf4b0049537647b5cd2d74ccbc1a78611134da66f63aa6b19>&format=json&callback=?', function(data) {
+    var crntPC = data.ipAddress
+    console.log(crntPC)
+        // next use ip address to find geo location
+        var getTheIP = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://free-geo-ip.p.rapidapi.com/json/" + crntPC,
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "free-geo-ip.p.rapidapi.com",
+                "x-rapidapi-key": "d82c676afemsh7edaf163ce44088p1641dajsn681616f67751"
+            }
+        }
+
+        $.ajax(getTheIP).done(function (theCrntLocation) {
+            console.log(theCrntLocation);
+            var theLat = theCrntLocation.latitude
+            var theLong = theCrntLocation.longitude
+            produceWeatherResults(theLat, theLong)  
+        });
+    });
+
+    console.log(window)
+    // define list containing of cities
     var cityList = $("#cityList")
     var yourList = [
-        ["New London, CT, US", 41.3556539, -72.0995209],
-        ["Groton, CT, US", 41.3556539, -72.0995209],
-        ["Kansas City, MO, USA", 39.0997265, -94.5785667]
+        ["New York, NY, USA", 40.7127753, -74.0059728],
     ];
+    // api call to open weather api
+    var APIKey = "19ebe7d8453b09616b508ab44e2e92b8";
+    // build a new list based on the your list array
     buildCityList(yourList)
 
 
@@ -21,17 +47,14 @@ $(document).ready(function () {
         var near_place = autocomplete.getPlace();
         var lati = near_place.geometry.location.lat()
         var logt = near_place.geometry.location.lng()
-        console.log(yourList)
+        // get the value of the searchbar contents
         var newCity = $("#searchInput").val()
+        // create a new array with the city information
         var newArray = new Array(newCity, lati, logt)
-        newCity = newArray
 
+        newCity = newArray
         yourList.push(newCity)
         console.log(yourList)
-        $("#latitudeInput").val(lati);
-        $("#longitudeInput").val(logt);
-        $("#latitudeView").text(lati);
-        $("#longitudeView").text(logt);
         // build new list item with city found in it
         // get the current value of the search bar
         buildCityList(yourList)
@@ -64,7 +87,6 @@ $(document).ready(function () {
                 // add the longitude attribute to the li
                 newLI.attr("long", yourList[i][2]);
                 // add a data-index to the li
-                
                 // append the new city to the 
                 cityList.append(newLI);
 
@@ -75,9 +97,23 @@ $(document).ready(function () {
             e.preventDefault();
             theLat = $(this).attr("lat")
             theLong = $(this).attr("long")
-            
-            // api call to weather
-            var APIKey = "19ebe7d8453b09616b508ab44e2e92b8";
+            produceWeatherResults(theLat, theLong)            
+
+        });
+
+        }
+
+     
+
+
+        // event listener for when the submit button is selected or enter is clicked
+        $("#searchInput").on("submit", function (e) {
+            e.preventDefault();
+            console.log("The enter button was selected")
+
+        });
+        
+function produceWeatherResults(theLat,theLong) {
 
             // Here we are building the URL we need to query the database
             var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+ theLat +"&lon="+ theLong +"&units=imperial&appid=" + APIKey;
@@ -113,20 +149,7 @@ $(document).ready(function () {
                 $("#cityDisc").text("Description: " + cityDisc);
 
             });
-        });
-
-        }
-
-     
-
-
-        // event listener for when the submit button is selected or enter is clicked
-        $("#searchInput").on("submit", function (e) {
-            e.preventDefault();
-            console.log("The enter button was selected")
-
-        });
-        
-
+    
+}
 
 });
